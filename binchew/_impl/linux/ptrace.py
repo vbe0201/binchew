9e571d8e50b50ae07b50a06609e0de7c51d26c6e
Valentin B. <valentin.be@protonmail.com>
2023-12-09 17:38:55 +0100

    Add support for Linux memory allocations

---
+++ binchew/_impl/linux/ptrace.py
@@ -0,0 +1,49 @@
+import ctypes
+
+from . import libc
+
+
+def ptrace_attach(pid: int):
+    libc.checked_ptrace(libc.PTRACE_ATTACH, pid, None, None)
+
+
+def ptrace_detach(pid: int):
+    libc.checked_ptrace(libc.PTRACE_DETACH, pid, None, None)
+
+
+def ptrace_single_step(pid: int):
+    libc.checked_ptrace(libc.PTRACE_SINGLESTEP, pid, None, None)
+
+
+def ptrace_continue(pid: int):
+    libc.checked_ptrace(libc.PTRACE_CONT, pid, None, None)
+
+
+def ptrace_interrupt(pid: int):
+    libc.checked_ptrace(libc.PTRACE_INTERRUPT, pid, None, None)
+
+
+def ptrace_get_regs(pid: int) -> libc.user_regs_struct:
+    regs = libc.user_regs_struct()
+    ptrace_update_regs(pid, regs)
+    return regs
+
+
+def ptrace_update_regs(pid: int, regs: libc.user_regs_struct):
+    libc.checked_ptrace(libc.PTRACE_GETREGS, pid, None, ctypes.byref(regs))
+
+
+def ptrace_set_regs(pid: int, regs: libc.user_regs_struct):
+    libc.checked_ptrace(libc.PTRACE_SETREGS, pid, None, ctypes.byref(regs))
+
+
+def ptrace_peek_text(pid: int, addr: int) -> int:
+    word = libc.ptrace(libc.PTRACE_PEEKTEXT, pid, ctypes.c_void_p(addr), None)
+    if word == -1:
+        libc.raise_errno_on_error()
+
+    return word
+
+
+def ptrace_poke_text(pid: int, addr: int, word: int):
+    libc.checked_ptrace(libc.PTRACE_POKETEXT, pid, ctypes.c_void_p(addr), word)
--- binchew/os/process/interface.py
+++ binchew/os/process/interface.py
@@ -52,3 +52,46 @@ class RawProcess:
         :raises OSError: Failed to access the described memory region.
         """
         raise NotImplementedError()
+
+    def allocate_memory(self, size: int) -> int:
+        """
+        Allocates memory in the target process.
+
+        This should not be used as a general-purpose memory allocator with small
+        allocation granularities.
+
+        .. info::
+            The underlying implementation has the semantics of `VirtualAlloc` on
+            Windows, or a in-process `mmap` syscall (eventually through `ptrace`)
+            on Linux.
+
+        .. warning::
+            Bears a risk of making the process behave unpredictably when misused.
+
+        :param size: The size of the memory allocation to make.
+
+        :raises OSError: Failed to allocate the requested memory in the process.
+        """
+        raise NotImplementedError()
+
+    def free_memory(self, addr: int, size: int):
+        """
+        Frees an allocation in the target process.
+
+        This should not be used as a general-purpose memory allocator with small
+        allocation granularities.
+
+        .. info::
+            The underlying implementation has the semantics of `VirtualFree` on
+            Windows, or an in-process `munmap` syscall (eventually through `ptrace`)
+            on Linux.
+
+        .. warning::
+            Bears a risk of making the process behave unpredictably when misused.
+        
+        :param addr: The address of a previously done memory allocation.
+        :param size: The exact size of the memory allocation.
+
+        :raises OSError: Failed to free the memory in the process.
+        """
+        raise NotImplementedError()
